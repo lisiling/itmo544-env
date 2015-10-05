7f8d377267cd975d33a8fb64bbe2befc75ea1cbb
李思岭 <lisiling@lisilings-MacBook-Pro.local>
2015-10-05 02:37:29 -0500

    Added code to pull new index.html from other githun account

--- install-env.sh
+++ install-env.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2
+sudo apt-get install -y apache2 git
+
+git clone https://github.com/lisiling/itmo-544-fall2015.git
+
+mv ./itmo-544-fall2015/images /var/www/html/images
+mv ./itmo-544-fall2015/index.html /var/www/html
 
 echo "Hello!" > /tmp/hello.txt
