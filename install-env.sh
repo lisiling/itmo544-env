f262d6c83b655821b47482183cb958ee271dbf38
李思岭 <lisiling@lisilings-MacBook-Pro.local>
2015-11-09 05:03:52 -0600

    updated

--- install-env.sh
+++ install-env.sh
@@ -7,5 +7,11 @@ git clone https://github.com/lisiling/itmo-544-fall2015.git
 
 mv ./itmo-544-fall2015/images /var/www/html/images
 mv ./itmo-544-fall2015/index.html /var/www/html
+mv ./itmo-544-2015fall/page2.html /var/www/html
+mv ./itmo-544-2015fall/*.php /var/www/html
 
 echo "Hello!" > /tmp/hello.txt
+
+
+
+
