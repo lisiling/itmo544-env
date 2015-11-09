0ccf275b5e4fa78c0eaa58c96e6e429cb702a914
李思岭 <lisiling@lisilings-MacBook-Pro.local>
2015-11-09 05:13:29 -0600

    Updated

--- install-webserver.sh
+++ install-webserver.sh
@@ -8,13 +8,15 @@ git clone https://github.com/lisiling/itmo-544-fall2015.git
 mv ./itmo-544-fall2015/images /var/www/html/images
 mv ./itmo-544-fall2015/index.html /var/www/html
 mv ./itmo-544-fall2015/*.php /var/www/html
+mv ./itmo-544-launch/setup.php /var/www/html
 
-curl -sS http://getcomposer.org/installer | php
+curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 
-php composer.phar require aws/aws-sdk-php
+sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
 
-mv vendor /var/www/html
+sudo mv vendor /var/www/html &> /tmp/movecomposer.txt
+
+sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
 
 echo "Hello!" > /tmp/hello.txt
-
 
