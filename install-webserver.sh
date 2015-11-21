da89b41eb80dfb0671da99a2b3dafc0bad77a7e0
李思岭 <lisiling@lisilings-MacBook-Pro.local>
2015-11-21 17:07:34 -0600

    add sns and publish

--- install-webserver.sh
+++ install-webserver.sh
@@ -20,3 +20,12 @@ sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
 
 echo "Hello!" > /tmp/hello.txt
 
+ARN=(`aws sns create-topic --name mp2`)
+
+echo "This is the ARN: $ARN"
+
+aws sns set-topic-attributes --topic-arn $ARN --attribute-name DisplayName --attribute-value mp2
+
+aws sns subscribe --topic-arn $ARN --protocol sms --notification-endpoint 13126782765
+
+aws sns publish --topic-arn arn:aws:sns:us-east-1:632259975581:mp2 --message "best code ever"
